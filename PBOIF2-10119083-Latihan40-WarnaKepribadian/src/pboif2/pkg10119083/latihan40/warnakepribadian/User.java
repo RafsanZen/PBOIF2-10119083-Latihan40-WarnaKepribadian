6f76ff8760e38daf6287b5969606d96a5dc2d4a2
Rafsan Zen M <73044188+RafsanZen@users.noreply.github.com>
2020-11-06 21:58:57 +0700

    Add files via upload

---
+++ PBOIF2-10119083-Latihan40-WarnaKepribadian/src/pboif2/pkg10119083/latihan40/warnakepribadian/User.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119083.latihan40.warnakepribadian;
+
+/**
+ *
+ * Nama : Rafsan Zen Mustaofa 
+ * NIM  : 10119083
+ * Kelas: IF-2
+ * Deskripsi: Program user
+ */
+public class User {
+        private String nama, warna;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+}
